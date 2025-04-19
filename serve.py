a5950ce88878e4b54f5dcf076dad62c4697d8214
Elimwas <90504113+Elionline2@users.noreply.github.com>
2025-04-19 16:39:50 +0300

    Update serve.py

--- serve.py
+++ serve.py
@@ -77,8 +77,9 @@ def serve_static(path):
         return "Page not found", 404
                 
 if __name__ == '__main__':
+    # This is only for local development.
     port = int(os.environ.get('PORT', 8080))
-    print(f"Starting on port {port}")
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
